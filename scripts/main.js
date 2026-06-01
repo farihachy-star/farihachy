05ad9ad761874a25126c2d6b6b11744343c0fb86
farihachowdhury009 <fariha@m-powersoftware.com.au>
2026-06-01 11:50:12 +1000

    Initial portfolio upload

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const menuBar = document.getElementById("menu_bar");
+    const navbar = document.getElementById("navbar");
+
+    // safety check (IMPORTANT)
+    if (menuBar && navbar) {
+        menuBar.addEventListener("click", () => {
+            navbar.classList.toggle("active");
+        });
+    }
+
+});
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+document.querySelectorAll(".navbar a").forEach(link => {
+    link.addEventListener("click", () => {
+        document.getElementById("navbar").classList.remove("active");
+    });
+});
